513df0549a2347b14d7a93e81d13cff21cb25b58
Andy Lee <andy5090@naver.com>
2019-08-09 20:15:32 +0900

    complete

--- src/api/User/createAccount/createAccount.js
+++ src/api/User/createAccount/createAccount.js
@@ -4,14 +4,18 @@ export default {
   Mutation: {
     createAccount: async (_, args) => {
       const { username, email, firstName = "", lastName = "", bio = "" } = args;
-      const user = await prisma.createUser({
+      const exists = await prisma.$exists.user({ username });
+      if (exists) {
+        throw Error("This username is already taken");
+      }
+      await prisma.createUser({
         username,
         email,
         firstName,
         lastName,
         bio
       });
-      return user;
+      return true;
     }
   }
 };
